e896bd92ecb566410e50b51ce6bbd1be7ddc6061
kevind <kevin.dominguez2002@hotmail.com>
2023-03-15 13:41:19 -0600

    Create component Gif

---
+++ src/responsesApi/getGifs.js
@@ -0,0 +1,16 @@
+const apiKey = 'Em0uzMe9TbVoEaEMhzobPmuij4erkoyd&q'
+
+export default function getGifs({keyword = 'cheetos'} = {}){
+    const apiURL = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}=${keyword}&limit=10&offset=0&rating=g&lang=en`
+    return fetch(apiURL)
+        .then(res => res.json()) 
+        .then(response => {
+            const{data} = response
+            const gifs = data.map(image => {
+                const {images, title, id} = image
+                const {url} = images.downsized_medium
+                return {title, id, url}
+            })
+            return gifs
+        })
+}
